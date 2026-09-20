a63803ce9069aa8c8e921f17e44c3f55e7a2ea5d
mroczect <mroczect@proton.me>
2026-09-20 15:00:33 +0700

    chore: remove tools

--- tools/graph/target/debug/deps/graph-b758e24192aacc20.d
+++
@@ -1,5 +0,0 @@
-/home/mroczect/pbl/pbl-trpl106-galonku/tools/graph/target/debug/deps/graph-b758e24192aacc20.d: src/main.rs
-
-/home/mroczect/pbl/pbl-trpl106-galonku/tools/graph/target/debug/deps/libgraph-b758e24192aacc20.rmeta: src/main.rs
-
-src/main.rs:
